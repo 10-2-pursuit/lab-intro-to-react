eac4d596a407377128a0ee85be0931b19a6f49be
salvagebar <salvagebar@gmail.com>
2023-07-25 22:24:26 -0400

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import UserProfile from "./Components/UserProfile";
 import "./index.css";
 
 function App() {
-  return <p>Hello, world!</p>;
+  return (
+    <>
+      <p>Hello, world!</p>
+      <UserProfile />
+    </>
+  );
 }
 
 export default App;
---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <div>
+            <h3>Navigational goodies here.</h3>
+        </div>
+    );
+}
+
+export default NavBar;
