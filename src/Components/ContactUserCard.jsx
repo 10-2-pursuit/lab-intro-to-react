0d9eab25069dcd58a864581c42896ad0d322b62b
Isaiah Desrosiers <40811090+IDGitHubMan@users.noreply.github.com>
2023-07-26 22:35:31 -0400

    Refactor to use UserProfile

--- src/Components/ContactUserCard.jsx
+++ src/Components/ContactUserCard.jsx
@@ -1,11 +1,9 @@
+import UserProfile from "./UserProfile";
 const ContactUserCard = () => {
   return (
-    <div>
-      <img src="../../public/profile.png" />
-      <h1>Name</h1>
-      <h2>Flavor</h2>
-      <p>Lorem Ipsum</p>
-    </div>
+    <li>
+      <UserProfile />
+    </li>
   );
 };
 
