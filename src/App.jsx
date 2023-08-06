7c131bb3ad1d89992c733525369d674b2d739ca3
AishaCKleemoff <96323559+AishaCKleemoff@users.noreply.github.com>
2023-08-06 12:06:59 -0400

    add more

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./index.css";
 import NavBar from "./Components/NavBar";
 import UserProfile from "./Components/UserProfile";
 import Posts from "./Components/Posts";
+import Contacts from "./Components/Contacts";
 
 function App() {
   return (
@@ -14,6 +15,9 @@ function App() {
         <section>
           <UserProfile />
           <Posts />
+        </section>
+        <section className="contacts">
+          <Contacts />
         </section>
       </main>
     </>
